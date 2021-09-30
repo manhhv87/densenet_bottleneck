0246c45b2b1ad46871fa65c5c1cfd399cf8c082f
manhhv87 <manhhv87@gmail.com>
2021-09-30 16:53:46 +0700

    densenet-121, change Adam() to Adamax, add Dropout

--- pretraining/trainer.py
+++ pretraining/trainer.py
@@ -195,7 +195,6 @@ if __name__ == '__main__':
         model = task_solver(task=args.task, arch=args.arch, stages=args.stages)
         model.add(tf.keras.layers.BatchNormalization())     # new adding
         model.add(tf.keras.layers.ReLU())   # new adding
-        model.add(tf.keras.layers.GlobalAvgPool1D())
 
         # # Adding global features learning part (new adding)
         # tf.keras.layers.LSTM(units=64, dropout=0.2, recurrent_dropout=0.1, return_sequences=True)
