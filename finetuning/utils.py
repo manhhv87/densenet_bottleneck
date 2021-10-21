09709f515b2f9acfcc64429415c34e33067d2fce
manhhv87 <manhhv87@gmail.com>
2021-10-21 11:11:31 +0700

    densenet-121

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -12,9 +12,9 @@ def ecg_feature_extractor(arch=None, stages=None):
     if arch is None or arch == 'resnet18':
         resnet = _DenseNet(num_outputs=None,
                            blocks=(6, 12, 24, 16)[:stages],
-                           first_num_channels=16,
-                           growth_rate=16,
-                           kernel_size=(3, 3, 3, 3),
+                           first_num_channels=964,
+                           growth_rate=642,
+                           kernel_size=(7, 5, 5, 3),
                            drop_rate=0,
                            include_top=False)  # not include fc layer
     elif arch == 'resnet34':
