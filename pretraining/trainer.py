fd64eb5f268ee480232a6996d5073c6b92bf8d40
manhhv87 <manhhv87@gmail.com>
2021-11-04 22:29:05 +0700

    densenet-121

--- pretraining/trainer.py
+++ pretraining/trainer.py
@@ -201,8 +201,8 @@ if __name__ == '__main__':
         # tf.keras.layers.GlobalAvgPool1D()
 
         # change from tf.keras.optimizers.RMSprop(learning_rate=0.0001)
-        model.compile(#optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0001),
-                      optimizer=tf.keras.optimizers.Adam(beta_1=0.9, beta_2=0.98, epsilon=1e-9),
+        model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.0005),
+                      #optimizer=tf.keras.optimizers.Adam(beta_1=0.9, beta_2=0.98, epsilon=1e-9),
                       loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                       metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name='acc')])
 
