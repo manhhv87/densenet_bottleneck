a557cc5e61fdce27935b82f1a2e5632a63860932
manhhv87 <manhhv87@gmail.com>
2021-11-05 08:37:51 +0700

    densenet-121

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -11,10 +11,10 @@ def ecg_feature_extractor(arch=None, stages=None):
     # See Table 1 in Deep Residual Learning for Image Recognition
     if arch is None or arch == 'resnet18':
         resnet = _DenseNet(num_outputs=None,
-                           blocks=(2, 0, 0, 0)[:stages],
+                           blocks=(1, 0, 0, 0)[:stages],
                            first_num_channels=32,
-                           growth_rate=16,
-                           kernel_size=(3, 3, 3, 3),
+                           growth_rate=32,
+                           kernel_size=(7, 5, 5, 3),
                            drop_rate=0,
                            include_top=False)  # not include fc layer
     elif arch == 'resnet34':
