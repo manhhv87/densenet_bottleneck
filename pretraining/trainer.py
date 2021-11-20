f1df183e73c0e1f9c965b1bd16b323d362b74f21
manhhv87 <manhhv87@gmail.com>
2021-11-20 11:17:52 +0700

    densenet-1

--- pretraining/trainer.py
+++ pretraining/trainer.py
@@ -206,7 +206,7 @@ if __name__ == '__main__':
 
         # change from tf.keras.optimizers.RMSprop(learning_rate=0.0001)
         model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                      optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+                      optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
                       metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name='acc')])
 
         # initialize the weights of the model
