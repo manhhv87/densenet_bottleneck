4087cefd03f66a57b7a70d27b1274e8fbc8412a8
manhhv87 <manhhv87@gmail.com>
2021-11-26 06:47:57 +0700

    re-format code

--- clr/config.py
+++ clr/config.py
@@ -3,8 +3,8 @@ import os
 
 # define the minimum learning rate, maximum learning rate, batch size,
 # step size, CLR method, and number of epochs
-MIN_LR = 1e-5
-MAX_LR = 1e-2
+MIN_LR = 1e-8
+MAX_LR = 2e-3
 STEP_SIZE = 8
 CLR_METHOD = "triangular2"
 
