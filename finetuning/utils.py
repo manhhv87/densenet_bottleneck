12f691d907e6a0249b2ca84c0a7913b92d8a1a59
manhhv87 <manhhv87@gmail.com>
2021-11-27 09:16:51 +0700

    re-format code

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -12,7 +12,7 @@ def ecg_feature_extractor(arch=None, stages=None):
     if arch is None or arch == 'resnet18':
         resnet = _DenseNet(num_outputs=None,
                            blocks=(6, 12, 24, 16)[:stages],
-                           first_num_channels=32,
+                           first_num_channels=16,
                            growth_rate=8,
                            kernel_size=(7, 5, 5, 3),
                            dropout_rate=None,
