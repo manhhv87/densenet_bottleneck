9d732fe4193db4ec52fbcc9bdeb25ec1a780a8b6
manhhv87 <manhhv87@gmail.com>
2021-11-30 21:04:16 +0700

    re-format code

--- transplant/modules/densenet1d.py
+++ transplant/modules/densenet1d.py
@@ -2,35 +2,47 @@ import tensorflow as tf
 
 
 class _DenseBlock(tf.keras.layers.Layer):
-    """A building block for a dense block.
-        # Arguments
-            growth_rate: float, growth rate at dense layers.
-            kernel_size: int, kernel size
-            bottleneck: bool, yes or no using bottleneck
-        # Returns
-            Output tensor for the block.
     """
-    def __init__(self, growth_rate, kernel_size, bottleneck=True, **kwargs):  # constructor
+        Creates a convolution block consisting of BN-ReLU-Conv.
+        Optional: bottleneck, dropout
+    """
+    def __init__(self, growth_rate, kernel_size, dropout_rate=None, bottleneck=False, **kwargs):  # constructor
         super().__init__(**kwargs)
         self.growth_rate = growth_rate
         self.kernel_size = kernel_size
         self.bottleneck = bottleneck
+        self.dropout_rate = dropout_rate
+        self.listLayers = []
 
     def build(self, input_shape):
+        # Bottleneck
         if self.bottleneck:
             self.bn = tf.keras.layers.BatchNormalization(epsilon=1.001e-5)
             self.relu = tf.keras.layers.Activation('relu')
-            self.conv = tf.keras.layers.Conv1D(filters=4 * self.growth_rate, kernel_size=1, use_bias=False)
+            self.conv = tf.keras.layers.Conv1D(4 * self.growth_rate, kernel_size=1, use_bias=False,
+                                               kernel_regularizer=tf.keras.regularizers.l2(1e-4))
+            self.listLayers.append(self.bn)
+            self.listLayers.append(self.relu)
+            self.listLayers.append(self.conv)
 
+            # Dropout
+            if self.dropout_rate:
+                self.drop = tf.keras.layers.Dropout(self.dropout_rate)
+                self.listLayers.append(self.drop)
+
+        # Standard (BN-ReLU-Conv)
         self.bn1 = tf.keras.layers.BatchNormalization(epsilon=1.001e-5)
         self.relu1 = tf.keras.layers.Activation('relu')
         self.conv1 = tf.keras.layers.Conv1D(filters=self.growth_rate, kernel_size=self.kernel_size,
                                             padding='same', use_bias=False)
+        self.listLayers.append(self.bn1)
+        self.listLayers.append(self.relu1)
+        self.listLayers.append(self.conv1)
 
-        if self.bottleneck:
-            self.listLayers = [self.bn, self.relu, self.conv, self.bn1, self.relu1, self.conv1]
-        else:
-            self.listLayers = [self.bn1, self.relu1, self.conv1]
+        # Dropout
+        if self.dropout_rate:
+            self.drop1 = tf.keras.layers.Dropout(self.dropout_rate)
+            self.listLayers.append(self.drop1)
 
         super().build(input_shape)
 
@@ -43,21 +55,25 @@ class _DenseBlock(tf.keras.layers.Layer):
 
 
 class _TransitionBlock(tf.keras.layers.Layer):
-    """A transition block.
-        # Arguments
-            num_filters: int, number of filters into transition layers.
-            reduction: float, compression rate at transition layers.
-        # Returns
-            output tensor for the block.
     """
-    def __init__(self, num_filters, **kwargs):
+        Creates a transition layer between dense blocks as transition, which do convolution and pooling.
+        Works as down-sampling.
+    """
+    def __init__(self, nb_channels, dropout_rate=None, **kwargs):
         super().__init__(**kwargs)
-        self.num_filters = num_filters
+        self.nb_channels = nb_channels
+        self.dropout_rate = dropout_rate
 
     def build(self, input_shape):
         self.bn = tf.keras.layers.BatchNormalization(epsilon=1.001e-5)
         self.relu = tf.keras.layers.Activation('relu')
-        self.conv = tf.keras.layers.Conv1D(self.num_filters, kernel_size=1, use_bias=False)
+        self.conv = tf.keras.layers.Conv1D(self.nb_channels, padding='same', kernel_size=1, use_bias=False,
+                                           kernel_regularizer=tf.keras.regularizers.l2(1e-4))
+
+        # Adding dropout
+        if self.dropout_rate:
+            self.drop = tf.keras.layers.Dropout(self.dropout_rate)
+
         self.avg_pool = tf.keras.layers.AveragePooling1D(pool_size=2, strides=2)
 
         super().build(input_shape)
@@ -66,6 +82,11 @@ class _TransitionBlock(tf.keras.layers.Layer):
         x = self.bn(x)
         x = self.relu(x)
         x = self.conv(x)
+
+        # Adding dropout
+        if self.dropout_rate:
+            x = self.drop(x)
+
         return self.avg_pool(x)
 
 
@@ -85,12 +106,14 @@ class _DenseNet(tf.keras.Model):
 
     def __init__(self, num_outputs=1, blocks=(6, 12, 24, 16), first_num_channels=64, growth_rate=32,
                  kernel_size=(3, 3, 3, 3), block_fn1=_DenseBlock, block_fn2=_TransitionBlock,
-                 bottleneck=True, include_top=True, **kwargs):  # constructor
+                 bottleneck=False, dropout_rate=None, include_top=True, **kwargs):  # constructor
 
         super().__init__(**kwargs)
 
         # Built Convolution layer
-        self.conv = tf.keras.layers.Conv1D(filters=first_num_channels, kernel_size=7, strides=2, use_bias=False)
+        self.conv = tf.keras.layers.Conv1D(filters=first_num_channels, kernel_size=7, padding='same',
+                                           kernel_regularizer=tf.keras.regularizers.l2(1e-4),
+                                           strides=2, use_bias=False)
         self.bn = tf.keras.layers.BatchNormalization(epsilon=1.001e-5)
         self.relu = tf.keras.layers.Activation('relu')
         self.maxpool = tf.keras.layers.MaxPooling1D(pool_size=3, strides=2, padding='same')
@@ -102,6 +125,7 @@ class _DenseNet(tf.keras.Model):
             for block in range(blocks[stage]):
                 self.densenet_blocks.append(block_fn1(growth_rate=growth_rate,
                                                       kernel_size=kernel_size[stage],
+                                                      dropout_rate=dropout_rate,
                                                       bottleneck=bottleneck))
 
             # This is the number of output channels in the previous dense block
@@ -111,7 +135,7 @@ class _DenseNet(tf.keras.Model):
             # between the dense blocks
             if stage != len(blocks) - 1:
                 num_channel_trans //= 2
-                self.densenet_blocks.append(block_fn2(num_filters=num_channel_trans))
+                self.densenet_blocks.append(block_fn2(num_filters=num_channel_trans, dropout_rate=dropout_rate))
 
         self.bn1 = tf.keras.layers.BatchNormalization(epsilon=1.001e-5)
         self.relu1 = tf.keras.layers.Activation('relu')
