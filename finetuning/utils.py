9bfbff8b348417ed25eb7f96b03b2bdb23f187cf
manhhv87 <manhhv87@gmail.com>
2021-12-11 08:00:04 +0700

    re-format code

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -14,7 +14,7 @@ def ecg_feature_extractor(arch=None, stages=None):
                            blocks=(6, 4, 0, 0)[:stages],
                            first_num_channels=16,
                            growth_rate=8,
-                           kernel_size=(8, 6, 6, 4),
+                           kernel_size=(8, 6, 4, 4),
                            bottleneck=True,
                            dropout_rate=0.05,
                            include_top=False)  # not include fc layer
