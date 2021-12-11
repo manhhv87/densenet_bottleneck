3d595445df9b15d85b3ce0a58452b9b1e062ade7
manhhv87 <manhhv87@gmail.com>
2021-12-11 08:53:04 +0700

    re-format code

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -16,7 +16,7 @@ def ecg_feature_extractor(arch=None, stages=None):
                            growth_rate=8,
                            kernel_size=(8, 6, 4, 4),
                            bottleneck=True,
-                           dropout_rate=0.01,
+                           dropout_rate=None,
                            include_top=False)  # not include fc layer
     elif arch == 'resnet34':
         resnet = _DenseNet(num_outputs=None,
