8da35f4f89e6232c25ded316d272dc96cb76804a
manhhv87 <manhhv87@gmail.com>
2021-12-21 10:03:02 +0700

    re-format code

--- clr/config.py
+++ clr/config.py
@@ -5,7 +5,7 @@ import os
 # step size, CLR method, and number of epochs
 MIN_LR = 1e-5
 MAX_LR = 1e-2
-STEP_SIZE = 4
+STEP_SIZE = 8
 CLR_METHOD = "triangular2"
 
 # define the path to the output learning rate finder plot, training
