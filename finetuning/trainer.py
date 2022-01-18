caa5f3029eca56546b19d13d2b538f6d036903c8
manhhv87 <manhhv87@gmail.com>
2022-01-18 22:29:12 +0700

    re-format code

--- finetuning/trainer.py
+++ finetuning/trainer.py
@@ -273,7 +273,38 @@ if __name__ == '__main__':
 
         print('[INFO] Train data shape:', data_set['x'].shape)
 
-        idx_data = np.arange(len(x))
+        print('[INFO] Building model ...')
+        num_classes = len(data_set['classes'])
+
+        if is_multiclass(data_set['y']):
+            activation = 'sigmoid'
+            loss = tf.keras.losses.BinaryCrossentropy()
+            accuracy = tf.keras.metrics.BinaryAccuracy(name='acc')
+        else:
+            activation = 'softmax'
+            loss = tf.keras.losses.CategoricalCrossentropy()
+            accuracy = tf.keras.metrics.CategoricalAccuracy(name='acc')
+
+        # not include fc layer
+        model = ecg_feature_extractor(arch=args.arch)
+        model.add(tf.keras.layers.Dense(units=num_classes, activation=activation))
+
+        # initialize the weights of the model
+        inputs = tf.keras.layers.Input(shape=data_set['x'].shape[1:], dtype=data_set['x'].dtype)
+        model(inputs)  # complete model
+
+        print('[INFO] Model parameters: {:,d}'.format(model.count_params()))
+
+        if args.weights_file:  # Sử dụng trọng số đã được pre-trained
+            # initialize weights (excluding the optimizer state) to load the pretrained resnet
+            # the optimizer state is randomly initialized in the `model.compile` function
+            print('[INFO] Loading weights from file {} ...'.format(args.weights_file))
+            model.load_weights(str(args.weights_file))
+
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=config.MIN_LR, beta_1=0.9,
+                                                         beta_2=0.98, epsilon=1e-9),
+                      loss=loss,
+                      metrics=[accuracy])
 
         kf = KFold(n_splits=args.k_fold, shuffle=True)
         foldNum = 0
@@ -281,6 +312,8 @@ if __name__ == '__main__':
         all_scores_f1_each_class = []
         all_scores_macro_f1 = []
         all_scores_macro_auc = []
+
+        idx_data = np.arange(len(x))
 
         for train_idx, val_idx in kf.split(idx_data):
             foldNum += 1
@@ -306,186 +339,77 @@ if __name__ == '__main__':
             val_size = len(val['x'])
             print('[INFO] Validation size {} ...'.format(val_size))
 
-            strategy = tf.distribute.MirroredStrategy()
+            callbacks = []
 
-            with strategy.scope():
-                print('[INFO] Building model ...')
-                num_classes = len(train['classes'])
-                print('num class {}'.format(num_classes))
+            logger = tf.keras.callbacks.CSVLogger(filename=str(args.job_dir / 'history.csv'))
+            callbacks.append(logger)
 
+            if args.val_metric in ['loss', 'acc']:
+                monitor = ('val_' + args.val_metric)  # if val else args.val_metric
+                checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
+                                                                monitor=monitor,
+                                                                save_best_only=True,
+                                                                save_weights_only=True,
+                                                                mode='auto',
+                                                                verbose=1)
+            elif args.val_metric == 'f1':
                 if is_multiclass(train['y']):
-                    activation = 'sigmoid'
-                    loss = tf.keras.losses.BinaryCrossentropy()
-                    accuracy = tf.keras.metrics.BinaryAccuracy(name='acc')
+                    score_fn = multi_f1
                 else:
-                    activation = 'softmax'
-                    loss = tf.keras.losses.CategoricalCrossentropy()
-                    accuracy = tf.keras.metrics.CategoricalAccuracy(name='acc')
+                    score_fn = f1
 
-                # not include fc layer
-                model = ecg_feature_extractor(arch=args.arch)
-                model.add(tf.keras.layers.Dense(units=num_classes, activation=activation))
+                checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
+                                              data=(val_data, val['y']),
+                                              # if val else (train_data, partial_train_data['y']),
+                                              score_fn=score_fn,
+                                              save_best_only=True,
+                                              verbose=1)
 
-                # initialize the weights of the model
-                inputs = tf.keras.layers.Input(shape=train['x'].shape[1:], dtype=train['x'].dtype)
-                model(inputs)  # complete model
+            elif args.val_metric == 'auc':
+                checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
+                                              data=(val_data, val['y']),
+                                              # if val else (train_data, partial_train_data['y']),
+                                              score_fn=auc,
+                                              save_best_only=True,
+                                              verbose=1)
 
-                print('[INFO] Model parameters: {:,d}'.format(model.count_params()))
+            elif args.val_metric == 'fmax':
+                checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
+                                              data=(val_data, val['y']),    # if val else (train_data, train['y']),
+                                              score_fn=f_max,
+                                              save_best_only=True,
+                                              verbose=1)
 
-                if args.weights_file:  # Sử dụng trọng số đã được pre-trained
-                    # initialize weights (excluding the optimizer state) to load the pretrained resnet
-                    # the optimizer state is randomly initialized in the `model.compile` function
-                    print('[INFO] Loading weights from file {} ...'.format(args.weights_file))
-                    model.load_weights(str(args.weights_file))
+            elif args.val_metric == 'fg':
+                checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
+                                              data=(val_data, val['y']),    # if val else (train_data, train['y']),
+                                              score_fn=challenge2020_metrics,
+                                              save_best_only=True,
+                                              verbose=1)
 
-                model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=config.MIN_LR,
-                                                                 beta_1=0.9, beta_2=0.98, epsilon=1e-9),
-                              loss=loss,
-                              metrics=[accuracy])
+            else:
+                raise ValueError('Unknown metric: {}'.format(args.val_metric))
 
-                callbacks = []
+            callbacks.append(checkpoint)
 
-                logger = tf.keras.callbacks.CSVLogger(filename=str(args.job_dir / 'history.csv'))
-                callbacks.append(logger)
+            early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50,
+                                                              min_delta=0.001, verbose=1)
+            callbacks.append(early_stopping)
 
-                if args.val_metric in ['loss', 'acc']:
-                    monitor = ('val_' + args.val_metric)  # if val else args.val_metric
-                    checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
-                                                                    monitor=monitor,
-                                                                    save_best_only=True,
-                                                                    save_weights_only=True,
-                                                                    mode='auto',
-                                                                    verbose=1)
-                elif args.val_metric == 'f1':
-                    if is_multiclass(train['y']):
-                        score_fn = multi_f1
-                    else:
-                        score_fn = f1
+            # otherwise, we have already defined a learning rate space to train over,
+            # so compute the step size and initialize the cyclic learning rate method
+            stepSize = config.STEP_SIZE * train_size // args.batch_size
+            clr = CyclicLR(mode=config.CLR_METHOD,
+                           base_lr=config.MIN_LR,
+                           max_lr=config.MAX_LR,
+                           step_size=stepSize)
 
-                    checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
-                                                  data=(val_data, val['y']),
-                                                  # if val else (train_data, partial_train_data['y']),
-                                                  score_fn=score_fn,
-                                                  save_best_only=True,
-                                                  verbose=1)
+            callbacks.append(clr)
 
-                elif args.val_metric == 'auc':
-                    checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
-                                                  data=(val_data, val['y']),
-                                                  # if val else (train_data, partial_train_data['y']),
-                                                  score_fn=auc,
-                                                  save_best_only=True,
-                                                  verbose=1)
-
-                elif args.val_metric == 'fmax':
-                    checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
-                                                  data=(val_data, val['y']),    # if val else (train_data, train['y']),
-                                                  score_fn=f_max,
-                                                  save_best_only=True,
-                                                  verbose=1)
-
-                elif args.val_metric == 'fg':
-                    checkpoint = CustomCheckpoint(filepath=str(args.job_dir / 'best_model.weights'),
-                                                  data=(val_data, val['y']),    # if val else (train_data, train['y']),
-                                                  score_fn=challenge2020_metrics,
-                                                  save_best_only=True,
-                                                  verbose=1)
-
-                else:
-                    raise ValueError('Unknown metric: {}'.format(args.val_metric))
-
-                callbacks.append(checkpoint)
-
-                early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=50,
-                                                                  min_delta=0.001, verbose=1)
-                callbacks.append(early_stopping)
-
-                # otherwise, we have already defined a learning rate space to train over,
-                # so compute the step size and initialize the cyclic learning rate method
-                stepSize = config.STEP_SIZE * train_size // args.batch_size
-                clr = CyclicLR(mode=config.CLR_METHOD,
-                               base_lr=config.MIN_LR,
-                               max_lr=config.MAX_LR,
-                               step_size=stepSize)
-
-                callbacks.append(clr)
-
-                # rlo = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=7,
-                #                                            verbose=1, min_lr=config.MIN_LR)
-                #
-                # callbacks.append(rlo)
-
-                print('[INFO] Training fold {}/{} ...'.format(foldNum, args.k_fold))
-                model.fit(train_data,
-                          epochs=args.epochs,
-                          verbose=1,
-                          validation_data=val_data,
-                          callbacks=callbacks)
-                print("============================================================================")
-
-                # load best model for inference
-                # print(
-                #     '[INFO] Loading the best weights from file {} ...'.format(str(args.job_dir / 'best_model.weights')))
-                # model.load_weights(filepath=str(args.job_dir / 'best_model.weights'))
-
-                # print('[INFO] Predicting training data for fold {} ...'.format(foldNum))
-                # train_y_prob = model.predict(x=train['x'], batch_size=args.batch_size)
-                # train_predictions = create_predictions_frame(y_prob=train_y_prob,
-                #                                              y_true=train['y'],
-                #                                              class_names=train['classes'],
-                #                                              record_ids=train['record_ids'])
-                # train_predictions.to_csv(path_or_buf=str(args.job_dir) + '/train_predictions_' + str(foldNum) + '.csv',
-                #                          index=False)
-
-        #         print('[INFO] Predicting validation data for fold {} ...'.format(foldNum))
-        #         val_y_prob = model.predict(x=val['x'], batch_size=args.batch_size)
-        #         val_predictions = create_predictions_frame(y_prob=val_y_prob,
-        #                                                    y_true=val['y'],
-        #                                                    class_names=train['classes'],
-        #                                                    record_ids=val['record_ids'])
-        #         val_predictions.to_csv(path_or_buf=str(args.job_dir) + '/val_predictions_' + str(foldNum) + '.csv',
-        #                                index=False)
-        #
-        #         val_pre = read_predictions(str(args.job_dir) + '/val_predictions_' + str(foldNum) + '.csv')
-        #         y_true = val_pre['y_true']
-        #         y_prob = val_pre['y_prob']
-        #
-        #         # Evaluation on F1
-        #         if args.val_metric == 'f1':
-        #             macro_f1 = f1(y_true, y_prob)
-        #             all_scores_macro_f1.append(macro_f1)
-        #             print('[INFO] macro f1 for fold {} is {}'.format(foldNum, macro_f1))
-        #             f1_each_class = f1_classes(y_true, y_prob)
-        #             all_scores_f1_each_class.append(f1_each_class)
-        #             print('[INFO] f1 for each class for fold {} is {}'.format(foldNum, f1_each_class))
-        #
-        #         # Evaluation on AUC
-        #         if args.val_metric == 'auc':
-        #             macro_auc = auc(y_true, y_prob)
-        #             all_scores_macro_auc.append(macro_auc)
-        #             print('[INFO] macro AUC for fold {} is {}'.format(foldNum, macro_auc))
-        #
-        #         # print('[INFO] Evaluates the model on the validation data ...')
-        #         # val_mse, val_mae = model.evaluate(val_data, verbose=1)
-        #         # all_scores_mse.append(val_mse)
-        #         # print('[INFO] Validation MSE for fold {} is {}'.format(foldNum, val_mse))
-        #         # print("============================================================================")
-        #
-        # scores_f1_each_class_array = np.concatenate(all_scores_f1_each_class, axis=0).reshape((5, 4))
-        #
-        # print("Results ...")
-        # print("============================================================================")
-        #
-        # if args.val_metric == 'f1':
-        #     print('[INFO] macro f1, mean and standard deviation values for all of folds is {}, {} and {}'.format(
-        #         all_scores_macro_f1, np.mean(all_scores_macro_f1), np.std(all_scores_macro_f1)))
-        #     print('[INFO] f1, mean and standard deviation values for each class of folds is {}, {} and {}'.format(
-        #         all_scores_f1_each_class, scores_f1_each_class_array.mean(axis=0), scores_f1_each_class_array.std(axis=0)))
-        #     # print('[INFO] mse, mean and standard deviation values for folds is {} and {}'.format(
-        #     #     all_scores_mse, np.mean(all_scores_mse), np.std(all_scores_mse)))
-        #
-        # if args.val_metric == 'auc':
-        #     print('[INFO] macro AUC, mean and standard deviation values for all of folds is {}, {} and {}'.format(
-        #         all_scores_macro_auc, np.mean(all_scores_macro_auc), np.std(all_scores_macro_auc)))
-        #     # print('[INFO] mse, mean and standard deviation values for folds is {} and {}'.format(
-        #     #     all_scores_mse, np.mean(all_scores_mse), np.std(all_scores_mse)))
+            print('[INFO] Training fold {}/{} ...'.format(foldNum, args.k_fold))
+            model.fit(train_data,
+                      epochs=args.epochs,
+                      verbose=1,
+                      validation_data=val_data,
+                      callbacks=callbacks)
+            print("============================================================================")
