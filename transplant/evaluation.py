d78311364368d8366a3a06f212c4256104178351
manhhv87 <manhhv87@gmail.com>
2022-01-23 23:23:49 +0700

    re-format code

--- transplant/evaluation.py
+++ transplant/evaluation.py
@@ -128,6 +128,7 @@ def f_beta_metric(y_true, y_prob, beta_f=2, beta_g=2, class_weights=None, single
     """ source: https://github.com/helme/ecg_ptbxl_benchmarking """
     # y_pred = y_pred >= 0.5
     y_pred = y_prob >= np.max(y_prob, axis=1)[:, None]
+    y_pred = y_pred.astype(int)
 
     print('[INFO] y_true = {}'.format(y_true))
     print('[INFO] y_pred = {}'.format(y_pred))
