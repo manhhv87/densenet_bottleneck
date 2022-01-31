93fa871393be17c866c21f204d2ef7305eb164c3
manhhv87 <manhhv87@gmail.com>
2022-01-31 09:24:18 +0700

    re-format code

--- transplant/evaluation.py
+++ transplant/evaluation.py
@@ -165,8 +165,7 @@ def challenge2020_scores(y_true, y_prob):
         for i in range(num_samples):
             if y_true[i, class_i] == y_pred[i, class_i] == 1:
                 A[class_i][class_i] += 1
-            # if y_pred[i, class_i] == 1 and y_true[i, class_i] != y_pred[i, class_i]:
-            else:
+            if y_true[i, class_i] == 0 and y_pred[i, class_i] == 1:
                 A[class_i][np.argmax(y_pred[i])] += 1
     return A
 
