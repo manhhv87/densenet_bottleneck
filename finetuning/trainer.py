106aa8b66e19172ae953dd2d9108691908423da7
manhhv87 <manhhv87@gmail.com>
2022-02-09 13:19:05 +0700

    re-format code

--- finetuning/trainer.py
+++ finetuning/trainer.py
@@ -312,6 +312,10 @@ if __name__ == '__main__':
             print('[INFO] Loading the best weights from file {} ...'.format(str(args.job_dir / 'best_model.weights')))
             model.load_weights(filepath=str(args.job_dir / 'best_model.weights'))
 
+            # Save the entire model as a SavedModel.
+            print('[INFO] Saving model ...')
+            model.save(str(args.job_dir / 'my_model'))
+
             print('[INFO] Predicting training data ...')
             train_y_prob = model.predict(x=train['x'], batch_size=args.batch_size)
             train_predictions = create_predictions_frame(y_prob=train_y_prob,
