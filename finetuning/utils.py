2a2f54a58d9306507d0c1dcf63b9a1bf075a6fe9
manhhv87 <manhhv87@gmail.com>
2022-02-15 23:54:10 +0700

    re-format code

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 from transplant.modules.densenet1d import _DenseNet
 
 
-def ecg_feature_extractor(arch=None, in_shape=(None, 16384, 1), stages=None):
+def ecg_feature_extractor(arch=None, in_shape=None, stages=None):
     # Furthermore, we use larger filter sizes (7, 5, 5, 3) at each stage respectively,
     # which we have observed to outperform the suggested smaller 3 × 3 filters.
     # See Table 1 in Deep Residual Learning for Image Recognition
     if arch is None or arch == 'resnet18':
-        resnet = _DenseNet(input_shape=in_shape,
+        resnet = _DenseNet(in_shape=in_shape,
                            num_outputs=None,
                            blocks=(6, 4, 6, 0)[:stages],
                            first_num_channels=16,
