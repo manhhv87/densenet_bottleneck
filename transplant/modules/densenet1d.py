f711e7fcaf290792fe8213a5e21e99edd1f50f5b
manhhv87 <manhhv87@gmail.com>
2022-02-15 23:57:19 +0700

    re-format code

--- transplant/modules/densenet1d.py
+++ transplant/modules/densenet1d.py
@@ -108,13 +108,13 @@ class _DenseNet(tf.keras.Model):
         include_top (bool) - yes or no include top layer
     """
 
-    def __init__(self, in_shape=None, num_outputs=1, blocks=(6, 12, 24, 16), first_num_channels=64, growth_rate=32,
+    def __init__(self, input_shape=(None, 16384, 1), num_outputs=1, blocks=(6, 12, 24, 16), first_num_channels=64, growth_rate=32,
                  kernel_size=(3, 3, 3, 3), block_fn1=_DenseBlock, block_fn2=_TransitionBlock,
                  bottleneck=False, dropout_rate=None, include_top=True, **kwargs):  # constructor
 
         super().__init__(**kwargs)
-        self.in_shape = in_shape
-        self.input_layer = tf.keras.layers.Input(shape=in_shape)
+        self.input_shape = input_shape
+        self.input_layer = tf.keras.layers.Input(shape=input_shape)
 
         # Built Convolution layer
         self.conv = tf.keras.layers.Conv1D(filters=first_num_channels, kernel_size=7, padding='same',
@@ -181,4 +181,4 @@ class _DenseNet(tf.keras.Model):
         return x
 
     def model(self):
-        return tf.keras.Model(inputs=[self.in_shape], outputs=self.call(self.in_shape))
+        return tf.keras.Model(inputs=[self.input_shape], outputs=self.call(self.input_shape))
