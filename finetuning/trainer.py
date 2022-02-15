becd62c1b942bc1257a0db1baaa61ba53104d137
manhhv87 <manhhv87@gmail.com>
2022-02-16 00:49:50 +0700

    re-format code

--- finetuning/trainer.py
+++ finetuning/trainer.py
@@ -171,12 +171,13 @@ if __name__ == '__main__':
                 accuracy = tf.keras.metrics.CategoricalAccuracy(name='acc')
 
             # not include fc layer
-            model = ecg_feature_extractor(arch=args.arch)
+            model = ecg_feature_extractor(arch=args.arch, input_layer=tf.keras.layers.Input(shape=train['x'].shape[1:],
+                                                                                            dtype=train['x'].dtype))
             model.add(tf.keras.layers.Dense(units=num_classes, activation=activation))
 
             # initialize the weights of the model
-            inputs = tf.keras.layers.Input(shape=train['x'].shape[1:], dtype=train['x'].dtype)
-            model(inputs)  # complete model
+            # inputs = tf.keras.layers.Input(shape=train['x'].shape[1:], dtype=train['x'].dtype)
+            # model(inputs)  # complete model
 
             print('[INFO] Model parameters: {:,d}'.format(model.count_params()))
 
