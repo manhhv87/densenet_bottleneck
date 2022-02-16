fbf7cc36779dfc572cb6476a727c203841bc6ede
manhhv87 <manhhv87@gmail.com>
2022-02-16 12:39:35 +0700

    re-format code

--- transplant/modules/densenet1d.py
+++ transplant/modules/densenet1d.py
@@ -175,7 +175,3 @@ class _DenseNet(tf.keras.Model):
             x = self.global_pool(x)
             x = self.classifier(x)
         return x
-
-    def build_graph(self, raw_shape):
-        x = tf.keras.layers.Input(shape=raw_shape)
-        return tf.keras.models.Model(inputs=[x], outputs=self.call(x))
