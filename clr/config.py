b4909110323332d1f9c83e900f6b1e1090cd14e7
manhhv87 <manhhv87@gmail.com>
2022-02-24 16:31:22 +0700

    change to draw Grad-CAM

--- clr/config.py
+++ clr/config.py
@@ -5,7 +5,7 @@ import os
 # step size, CLR method, and number of epochs
 MIN_LR = 1e-5
 MAX_LR = 1e-3
-STEP_SIZE = 4
+STEP_SIZE = 8
 CLR_METHOD = "triangular"
 
 # define the path to the output learning rate finder plot, training
