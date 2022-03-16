2c2305d5c614595dc95053dc5c2194377fbe5d38
manhhv87 <manhhv87@gmail.com>
2022-03-16 23:15:23 +0700

    DenseNet-39

--- finetuning/utils.py
+++ finetuning/utils.py
@@ -8,17 +8,17 @@ from transplant.modules.densenet1d import _DenseNet
 def ecg_feature_extractor(input_layer=None, stages=None):
     backbone_model = _DenseNet(input_layer=input_layer,
                                num_outputs=None,
-                               # blocks=(6, 4, 6, 0)[:stages],      # Own model
+                               blocks=(6, 4, 6, 0)[:stages],      # Own model
                                # blocks=(6, 12, 24, 16)[:stages],   # DenseNet-121
                                #blocks=(6, 12, 32, 32)[:stages],     # DenseNet-169
-                               blocks=(6, 12, 48, 32)[:stages],  # DenseNet-201
+                               # blocks=(6, 12, 48, 32)[:stages],  # DenseNet-201
                                # blocks=(6, 12, 64, 48)[:stages],  # DenseNet-264
-                               # first_num_channels=16,
-                               first_num_channels=64,
-                               # growth_rate=8,
-                               growth_rate=32,
-                               # kernel_size=(8, 6, 8, 4),
-                               kernel_size=(3, 3, 3, 3),
+                               first_num_channels=16,
+                               # first_num_channels=64,
+                               growth_rate=8,
+                               # growth_rate=32,
+                               kernel_size=(8, 6, 8, 4),
+                               # kernel_size=(3, 3, 3, 3),
                                bottleneck=True,
                                dropout_rate=None,
                                include_top=False).model()
