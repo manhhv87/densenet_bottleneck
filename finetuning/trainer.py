6744e1e2347e406ebdc3d03459f9d0f7d893ba78
manhhv87 <manhhv87@gmail.com>
2022-04-27 14:28:41 +0700

    DenseNet-37

--- finetuning/trainer.py
+++ finetuning/trainer.py
@@ -195,11 +195,12 @@ if __name__ == '__main__':
 
             x = tf.keras.layers.BatchNormalization()(backbone_model.output)
             x = tf.keras.layers.Activation('relu')(x)
-            x = Bidirectional(LSTM(units=64, dropout=0.5, recurrent_dropout=0.1)(x))
+            x = Bidirectional(LSTM(units=64, return_sequences=True, dropout=0.5, recurrent_dropout=0.1))(x)
             # x = tf.keras.layers.GlobalMaxPooling1D()(x)
             x = tf.keras.layers.Dense(units=64, activation='relu')(x)
             x = tf.keras.layers.Dense(units=num_classes, activation=activation)(x)
             model = tf.keras.models.Model(inputs=backbone_model.input, outputs=x)
+            model.summary()
 
             print('[INFO] Model parameters: {:,d}'.format(model.count_params()))
 
