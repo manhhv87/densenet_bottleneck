34a4a910a7638b6a5ff115801c5a6d9137beda19
manhhv87 <manhhv87@gmail.com>
2022-05-12 22:34:59 +0700

    DenseNet-BiLSTM-GAP

--- finetuning/trainer.py
+++ finetuning/trainer.py
@@ -191,11 +191,11 @@ if __name__ == '__main__':
             backbone_model = ecg_feature_extractor(input_layer=inputs)
             # x = tf.keras.layers.GlobalMaxPooling1D()(backbone_model.output)
             # x = tf.keras.layers.GlobalAveragePooling1D()(backbone_model.output)
-            x = tf.keras.layers.Flatten()(backbone_model.output)
+            # x = tf.keras.layers.Flatten()(backbone_model.output)
 
-            # x = Bidirectional(LSTM(units=64, return_sequences=True))(backbone_model.output)
+            x = Bidirectional(LSTM(units=64, return_sequences=True))(backbone_model.output)
             # x = tf.keras.layers.GlobalMaxPooling1D()(x)
-            # x = tf.keras.layers.GlobalAveragePooling1D()(x)
+            x = tf.keras.layers.GlobalAveragePooling1D()(x)
 
             x = tf.keras.layers.Dense(units=num_classes, activation=activation)(x)
             model = tf.keras.models.Model(inputs=backbone_model.input, outputs=x)
